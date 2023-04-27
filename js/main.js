b03822ff94272ea513a0f0af96dc1d78652abfb6
FabrizioFavia <personal.favia@gmail.com>
2023-04-27 15:38:45 +0200

    feat(html/css/js): created basic srtructure.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+let cellContainer = document.getElementById("grid");
+let startBtn = document.getElementById("start");
+let gridWidth = 49;
+
+function difficultySelect(value) {
+
+    gridWidth = value;
+}
+
+startBtn.addEventListener("click", function () {
+
+    cellContainer.innerHTML = "";
+
+    for (let i = 1; i <= gridWidth; i++) {
+
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+
+        if (gridWidth == 49) {
+            cell.classList.add("easyCell");
+        }
+
+        if (gridWidth == 81) {
+            cell.classList.add("mediumCell");
+        }
+
+        if (gridWidth == 100) {
+            cell.classList.add("hardCell");
+        }
+
+        cell.append(i);
+
+        cell.addEventListener("click", function () {
+            this.classList.toggle("bckgBlue");
+            console.log(this.innerText)
+        })
+
+        cellContainer.appendChild(cell);
+    }
+})
