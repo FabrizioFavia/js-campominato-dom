91a02c80cab1a8e4e18a61c9782675cd8e9d828d
FabrizioFavia <personal.favia@gmail.com>
2023-04-27 16:11:38 +0200

    feat(js): created function getBombNumber.

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 let cellContainer = document.getElementById("grid");
 let startBtn = document.getElementById("start");
-let gridWidth = 49;
+let cellNumber = 49;
 
 function difficultySelect(value) {
 
-    gridWidth = value;
+    cellNumber = value;
 }
 
 startBtn.addEventListener("click", function () {
 
     cellContainer.innerHTML = "";
 
-    for (let i = 1; i <= gridWidth; i++) {
+    for (let i = 1; i <= cellNumber; i++) {
 
         const cell = document.createElement("div");
         cell.classList.add("cell");
 
-        if (gridWidth == 49) {
+        if (cellNumber == 49) {
             cell.classList.add("easyCell");
         }
 
-        if (gridWidth == 81) {
+        if (cellNumber == 81) {
             cell.classList.add("mediumCell");
         }
 
-        if (gridWidth == 100) {
+        if (cellNumber == 100) {
             cell.classList.add("hardCell");
         }
 
@@ -32,9 +32,24 @@ startBtn.addEventListener("click", function () {
 
         cell.addEventListener("click", function () {
             this.classList.toggle("bckgBlue");
-            console.log(this.innerText)
+            console.log(this.innerText);
         })
 
         cellContainer.appendChild(cell);
     }
+
+
 })
+
+function getBombNumber(cellNumber) {
+
+    let bomb;
+
+    for (let i = 1; i <= 16; i++) {
+        bomb = (Math.floor(Math.random() * cellNumber));
+        console.log(bomb);
+    }
+    console.log(`Il numero di celle è: ${cellNumber}`)
+    return bomb;
+
+}
